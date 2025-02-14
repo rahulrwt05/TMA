c980beabbc759967c717900b9b91a6ddfe6537aa
rahulrwt05 <rahulmarch2002@gmail.com>
2025-02-14 23:21:14 +0530

    added files

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://tma-ten.vercel.app/login",
+    origin: "https://tma-ten.vercel.app",
     credentials: true,
   })
 );
